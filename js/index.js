31c001ecaef372f806c600abf804111cc0794074
Ting Chung <tchung682@gmail.com>
2020-05-14 19:26:28 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 var element = document.createElement('div');
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
+document.body.appendChild(element)
 var ul = document.createElement('ul');
 
 for (let i = 0; i < 3; i++) {
